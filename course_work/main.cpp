8ea8ba3f33e9ba30dc0d79a3187decfb679b4903
Ilya Kangin <illlyakangin@gmail.com>
2021-05-27 17:34:46 +0300

    Refactored serial version, problem with saving empty data in the last element of dictionary

--- course_work/main.cpp
+++ course_work/main.cpp
@@ -10,9 +10,10 @@
 #include <filesystem>
 #include <vector>
 #include <map>
+#include <sstream>
 
 using namespace std;
-//namespace fs = std::filesystem;
+
 
 struct myStruct {
     string path;
@@ -20,12 +21,42 @@ struct myStruct {
 };
 
 
+void print_map (map <string, vector <myStruct> > dict) {
+    
+    for(map<string, vector<myStruct> >::iterator ii=dict.begin(); ii!=dict.end(); ++ii){
+        
+           cout << (*ii).first << ":\n";
+        
+           vector <myStruct> vect = (*ii).second;
+        
+           for (unsigned j = 0; j < vect.size(); j++){
+               cout << vect[j].path << " -- position " << vect[j].position << endl;
+           }
+           cout << endl;
+    }
+}
+
+string delete_punctuation(string line){
+
+    
+    for_each(line.begin(), line.end(), [](char & c){
+        if(ispunct(c) && c != '\'' && c != '\n'){
+            c = ' ';
+        }
+        c = ::tolower(c);
+    });
+    
+    return line;
+}
+
+void fill_map(){
+    
+}
+
 int main(int argc, const char * argv[]) {
     
-    string path = "/Users/i.kangin/Downloads/course_work/datasets";
-    string filepath;
+    string path = "/Users/i.kangin/Downloads/course_work/datasets/";
     string line;
-    string temp;
     
     myStruct w;
     
@@ -33,54 +64,43 @@ int main(int argc, const char * argv[]) {
     
     for(auto& p: std::__fs::filesystem::recursive_directory_iterator(path)) {
         
-        std::cout << p.path() << endl;
-        
         ifstream myfile (p.path());
-                
         w.path = p.path();
         
         if (myfile.is_open()) {
             getline(myfile, line);
             
-            std::for_each(line.begin(), line.end(), [](char & c){
-                if(ispunct(c)){
-                    c = '\0';
-                }
-                c = ::tolower(c);
-            });
+            cout << line << endl;
+            
+            line = delete_punctuation(line);
+            
+            cout << line << endl;
             
             
             int itr = 1;
             
             string word = "";
-            for (auto x : line){
-                if (x == ' ') {
+
+            stringstream sstr(line);
+            
+            while (sstr >> word) {
+                if (word != "br" && !word.empty() && word != "\0"){
                     w.position = itr;
                     dictionary[word].push_back(w);
-                    
-                    cout << word << endl;
-                    word = "";
                     itr++;
-                } else {
-                    word = word + x;
+                    cout << word << endl;
                 }
             }
+            
             
         } else {
             cout << "cannot open file" << endl;
         }
     }
     
-    cout << endl << endl;
-     
-    for(map<string, vector<myStruct> >::iterator ii=dictionary.begin(); ii!=dictionary.end(); ++ii){
-           cout << (*ii).first << ":\n";
-           vector <myStruct> inVect = (*ii).second;
-           for (unsigned j=0; j<inVect.size(); j++){
-               cout << inVect[j].path << " -- position" << inVect[j].position << endl;
-           }
-           cout << endl;
-    }
+         
+    print_map(dictionary);
+    
 
     return 0;
 }
