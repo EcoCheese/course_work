e739154ca5c42adc9cb85d0c3b895af1a1b86b21
Ilya Kangin <illlyakangin@gmail.com>
2021-05-28 03:05:07 +0300

    Added time counting for both methods. Added check for only .txt files. Cleaned up lines.

--- course_work/main.cpp
+++ course_work/main.cpp
@@ -12,6 +12,7 @@
 #include <map>
 #include <thread>
 #include <sstream>
+#include <ctime>
 
 using namespace std;
 
@@ -22,7 +23,7 @@ struct myStruct {
 };
 
 map<string,vector<myStruct>> dictionary;
-//pthread_mutex_t m;
+pthread_mutex_t m;
 
 
 void print_map (map <string, vector <myStruct> > dict) {
@@ -58,8 +59,14 @@ void open_serial_files(string path, myStruct ms){
     
     string line;
     
+    std::clock_t start;
+    start = std::clock();
+    
     for(auto& p: std::__fs::filesystem::recursive_directory_iterator(path)) {
         
+        if (p.path().extension() != ".txt")
+            continue;
+    
         ifstream myfile (p.path());
         ms.path = p.path();
         
@@ -86,6 +93,8 @@ void open_serial_files(string path, myStruct ms){
             cout << "cannot open file" << endl;
         }
     }
+    
+    std::cout << "Time of serial: " << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
 }
 
 //MARK: Multi
@@ -93,53 +102,54 @@ void open_serial_files(string path, myStruct ms){
 struct INPUT {
     int start;
     int end;
-//    string path;
     myStruct ms;
-    vector<string> ppp; //paths
+    vector<string> paths;
 };
 
 void *newFunc(void * arguments){
     
-//    pthread_mutex_lock(&m);
     
     struct INPUT *in = (struct INPUT *)arguments;
     
     string line;
-    ifstream myfile (in->ppp.at(in->start));
-    in->ms.path = in->ppp.at(in->start);
+    ifstream myfile ;
     
-    if (myfile.is_open()) {
+    
+    
+    for (int i = in->start; i < in->end; i++){
+        myfile.open(in->paths.at(i));
+        in->ms.path = in->paths.at(i);
         
-        getline(myfile, line);
+        if (myfile.is_open()) {
+            
+            
+            getline(myfile, line);
 
-        line = delete_punctuation(line);
-        int itr = 1;
-        string word = "";
-        stringstream sstr(line);
-        
-        
-        
-        while (sstr >> word) {
-            if (word != "br" && !word.empty() && word != "\0"){
-                in->ms.position = itr;
-                
-                dictionary[word].push_back(in->ms);
-                
-                itr++;
+            line = delete_punctuation(line);
+            int itr = 1;
+            string word = "";
+            stringstream sstr(line);
+            
+            pthread_mutex_lock(&m);
+            
+            while (sstr >> word) {
+                if (word != "br" && !word.empty() && word != "\0"){
+                    in->ms.position = itr;
+                    dictionary[word].push_back(in->ms);
+                    itr++;
+                }
             }
+            
+            pthread_mutex_unlock(&m);
+            
+            myfile.close();
+            
+        } else {
+            cout << "cannot open file" << endl;
         }
         
-
         
-        myfile.close();
-        
-        
-        
-    } else {
-        cout << "cannot open file" << endl;
     }
-    
-//    pthread_mutex_unlock(&m);
     
     return NULL;
 }
@@ -148,44 +158,52 @@ void *newFunc(void * arguments){
 
 void multi(myStruct ms, string path){
     
-    pthread_t threads[2];
+    pthread_mutex_init(&m, NULL);
     
+    int t, rc;
     
+    cout << "Enter number of threads (>1):";
+    cin >> t;
+    t--;
     
-//    pthread_mutex_init(&m, NULL);
+    if(t == 0){
+        cout << "Using serial version instead!" << endl;
+        open_serial_files(path, ms);
+        return;
+    }
     
-    int rc, n = 2;
+    pthread_t threads[t];
+    
     vector<string>* Paths = new vector<string>;
 
-    for (auto& p : std::__fs::filesystem::directory_iterator(path)){
+    for (auto& p : std::__fs::filesystem::recursive_directory_iterator(path)){
+        if (p.path().extension() != ".txt")
+            continue;
         Paths->push_back(p.path().string());
     }
     
-    int size = Paths->size();
+    struct INPUT in[Paths->size()];
     
-    struct INPUT in[size];
-    
+    std::clock_t start;
+    start = std::clock();
 
-    
-    
-    for(int i = 0; i < 2; i++){
-//        in.path = Paths->at(i);
-//        in = malloc(sizeof(INPUT));
-        in[i].ppp = *Paths;
-        in[i].start = Paths->size() / (float)n * i;
-        in[i].end = Paths->size() / (float)n * (i + 1);
+    for(int i = 0; i < t; i++){
+        in[i].paths = *Paths;
+        in[i].start = Paths->size() / (float)t * i;
+        in[i].end = Paths->size() / (float)t * (i + 1);
         rc = pthread_create(&threads[i], NULL, &newFunc, (void *)&in[i]);
         if (rc != 0) {
             printf("Thread creation failed");
             exit(-1);
         }
     }
-    
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < t; i++) {
         pthread_join(threads[i], NULL);
     }
-
-//    pthread_mutex_destroy(&m);
+    
+    pthread_mutex_destroy(&m);
+    
+    std::cout << "Time of multi: " << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
 }
 
 //MARK: Lookup query
@@ -207,35 +225,31 @@ void find_query (string query) {
 
 int main(int argc, const char * argv[]) {
     
-    string path = "/Users/i.kangin/Downloads/course_work/datasets/";
+    string path = "/Users/i.kangin/Downloads/course_work/datasets";
     int var = 0;
     bool created = false;
     string query = "";
     myStruct w;
     
-    
     while (true) {
         
-        cout << endl << "Choose variant:\n\t1 - serial\n\t2 - multi\n\t3 - find query" << endl;
-        
+        cout << endl << "Choose variant:\n\t1 - serial\n\t2 - multi\n\t3 - find query\n\t4 - erase dictionary\n\t5 - print dictionary" << endl;
         cin >> var;
         
         switch (var) {
             case 1:
-                
                 open_serial_files(path, w);
-                print_map(dictionary);
+//                print_map(dictionary);
                 created = true;
                 break;
                 
             case 2:
                 multi(w, path);
-                print_map(dictionary);
+//                print_map(dictionary);
                 created = true;
                 break;
                 
             case 3:
-                
                 if (created) {
                     cout << "Enter query: ";
                     cin >> query;
@@ -244,18 +258,21 @@ int main(int argc, const char * argv[]) {
                 } else {
                     cout << "Dictionary wasn't formed yet, try again later...";
                 }
-               
-                
                 break;
                 
             case 4:
                 dictionary.clear();
+                created = false;
+                cout << "Dictionary erased.";
+                break;
+                
+            case 5:
+                print_map(dictionary);
                 break;
                 
             default:
                 break;
         }
     }
-    
     return 0;
 }
